342d16c3f9d4b72448d84fb2afa9eebbfecf2a10
Johan <johan.loison@yahoo.com>
2021-09-20 10:06:38 +0200

    création classe et pajeJeu

---
+++ src/app/model/session-ressource.spec.ts
@@ -0,0 +1,7 @@
+import { SessionRessource } from './session-ressource';
+
+describe('SessionRessource', () => {
+  it('should create an instance', () => {
+    expect(new SessionRessource()).toBeTruthy();
+  });
+});
